c42f09ebe36ac8f10f1efe2e7f369f468be40757
Jaroslaw Hirniak <j@hirniak.info>
2015-06-23 21:13:26 +0200

    Moved website to separate directory, added logo.

--- website/gulpfile.js
+++ website/gulpfile.js
@@ -27,3 +27,25 @@ wrench.readdirSyncRecursive('./gulp').filter(function(file) {
 gulp.task('default', ['clean'], function () {
   gulp.start('build');
 });
+
+
+var gulp = require('gulp');
+var gls = require('gulp-live-server');
+gulp.task('serve', function() {
+  //1. serve with default settings
+  var server = gls.static(); //equals to gls.static('public', 3000);
+  server.start();
+
+  //2. serve at custom port
+  var server = gls.static('dist', 8888);
+  server.start();
+
+  //3. serve multi folders
+  var server = gls.static(['dist', '.tmp']);
+  server.start();
+
+  //use gulp.watch to trigger server actions(notify, start or stop)
+  gulp.watch(['static/**/*.css', 'static/**/*.html'], function () {
+    server.notify.apply(server, arguments);
+  });
+});
